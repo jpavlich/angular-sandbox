fb74e959ee31a268f1385e2636994b097565289e
jpavlich <jpavlich@javeriana.edu.co>
2017-11-24 17:40:55 -0500

    .

--- hot-test/src/app/app.component.ts
+++ hot-test/src/app/app.component.ts
@@ -7,7 +7,7 @@ import * as Handsontable from 'handsontable';
 const CustomEditor1 = Handsontable.editors.AutocompleteEditor.prototype.extend();
 
 
-CustomEditor1.prototype.queryChoices = function(query) {
+CustomEditor1.prototype.queryChoices = function (query) {
   this.query = query;
   const source = this.cellProperties.source;
 
@@ -27,12 +27,12 @@ CustomEditor1.prototype.queryChoices = function(query) {
 };
 
 CustomEditor1.prototype.stripValuesIfNeeded = function (values) {
-  const {allowHtml} = this.cellProperties;
+  const { allowHtml } = this.cellProperties;
 
-  const stringifiedValues = values.map((value) => JSON.stringify(value));
+  const stringifiedValues = values;// values.map((value) => value.id);
   // const strippedValues = arrayMap(stringifiedValues, (value) => (allowHtml ? value : stripTags(value)));
   const strippedValues = stringifiedValues; // TODO Remover tags html
-  console.log('--->');//, strippedValues);
+  console.log(values);
   return strippedValues;
 };
 
@@ -51,6 +51,7 @@ export class AppComponent {
 
   title = 'app';
 
+
   data = [
     [1, 2, 3],
     [4, 5, 6],
@@ -63,8 +64,8 @@ export class AppComponent {
       editor: CustomEditor1,
       source: [
         { id: 1, value: 'aaa' },
-        { id: 2, value: 'bbb' },
-        { id: 3, value: 'ccc' },
+        { id: 4, value: 'bbb' },
+        { id: 7, value: 'ccc' },
       ]
     },
     {
@@ -74,13 +75,45 @@ export class AppComponent {
     }
   ];
 
-  dropdownRenderer(hotInstance, td, row, column, prop, value, cellProperties) {
-    const data = JSON.parse(value);
-    Handsontable.renderers.AutocompleteRenderer.apply(this, [hotInstance, td, row, column, prop, data.value, cellProperties]);
-    td.style.fontWeight = 'bold';
-    td.style.textAlign = 'center';
+  options = {
+    columnSorting: true,
+    contextMenu: [
+      'row_above', 'row_below', 'remove_row'
+    ]
+  };
 
-    return td;
+  dropdownRenderer(hotInstance: HotTable, td, row, column, prop, value, cellProperties) {
+    Handsontable.renderers.BaseRenderer.apply(this, arguments);
+    const valueMap = cellProperties.source.reduce(function (map, obj) {
+      map[obj.id] = obj.value;
+      return map;
+    }, {});
+
+    if (td.children.length > 0) {
+      td.removeChild(td.firstChild);
+    }
+    const selectList = document.createElement('select');
+    td.appendChild(selectList);
+
+
+    cellProperties.source.forEach(d => {
+      const option = document.createElement('option');
+      option.value = d.id;
+      option.text = d.value;
+      selectList.appendChild(option);
+    });
+    selectList.value = value;
+    selectList.onchange = (event) => {
+      console.log(event);
+      // const sourceData = hotInstance.getHandsontableInstance().getSourceDataAtCell(row, prop);
+    // TODO See https://github.com/tdjun/Handsontable-autocomplete2/blob/master/autocomplete2.editor.js
+
+
+    };
+  }
+
+  afterChange(event) {
+    console.log(event);
   }
 
 }
