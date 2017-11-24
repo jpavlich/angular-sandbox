36076a23413211e96116758f8af6c7c6c81ffae3
jpavlich <jpavlich@javeriana.edu.co>
2017-11-24 10:36:21 -0500

    .

--- hot-test/src/app/app.component.ts
+++ hot-test/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
-import {HotTable} from 'ng2-handsontable';
+import { Component, ViewChild } from '@angular/core';
+import { HotTable } from 'ng2-handsontable';
+import * as Handsontable from 'handsontable';
 
 
-declare function registerCustomDropdown(hot);
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
+
 
   @ViewChild(HotTable) hotTableComponent;
 
@@ -21,22 +25,37 @@ export class AppComponent implements AfterViewInit {
     [7, 8, 9]
   ];
 
-  columns = [{
-    data: 'UserName', // from datasource
-    editor: 'select2',
-    select2Options: { // these options are the select2 initialization options
-      data: [{ id: 1, text: 'jsmith' }, { id: 2, text: 'wjones' }],
-      dropdownAutoWidth: true,
-      allowClear: true,
-      width: 'resolve'
+  // columns = [
+  //   {
+  //     renderer: 'dropdown2',
+  //     source: ['yellow', 'red', 'orange', 'green', 'blue', 'gray', 'black', 'white']
+  //   },
+  //   {
+
+  //   },
+  //   {
+  //   }
+  // ];
+
+
+
+  options = {
+    cells: (row, col, prop) => {
+      const cellProperties = {};
+      if (row > 0 && col === 0) {
+        Object.assign(cellProperties, {renderer: this.customRenderer});
+        return cellProperties;
+      }
+      return prop;
     }
+
+  };
+
+
+  customRenderer(hotInstance, td, row, column, prop, value, cellProperties) {
+    Handsontable.renderers.BaseRenderer.apply(this, arguments);
+    td.style.fontWeight = 'bold';
+    td.style.textAlign = 'center';
   }
-  ];
 
-  options = {};
-
-  ngAfterViewInit() {
-    registerCustomDropdown(this.hotTableComponent.getHandsontableInstance());
-
-  }
 }
